95a6da1743d3b8adc7cee6a76ddd426b691a3764
Meralim YG <roman2004431@gmail.com>
2023-12-07 08:49:30 +0200

    feat/RG: add imports of components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import ImagePlacer from "./components/ImagePlacer";
+import DescriptionBox from "./components/DescriptionBox";
+import Card from "./components/Card";
+import HorizontalStack from "./components/HorizontalStack";
+
 
 function App() {
-
   return (
     <>
-     
+      <HorizontalStack>
+        
+      </HorizontalStack>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
