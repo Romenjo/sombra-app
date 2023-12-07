ca1066f44dfc2fa1041583379bfcd925598079b9
AnnaHuk05 <annavonhuk@gmail.com>
2023-12-07 13:58:19 +0200

    feat/HA: add Huk profile to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
+
 import "./App.css";
-import ImagePlacer from "./components/ImagePlacer";
-import DescriptionBox from "./components/DescriptionBox";
 import Card from "./components/Card";
 import HorizontalStack from "./components/HorizontalStack";
+import HukannaImage from "./images/Hukanna.jpg";
 
 
 function App() {
   return (
     <>
       <HorizontalStack>
+        <Card
+          imageUrl={HukannaImage}
+          name="HukannaImage"
+          description="Student of LPNU, 18, 166. Likes music, dances and Sombra Lessons.
+          Listens to pop tracks."
+          glowColor="yellow"
+          />
         
       </HorizontalStack>
     </>
