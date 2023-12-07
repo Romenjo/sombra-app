1763244a0246392e92f7a1a22edfa70bb217426d
Meralim YG <roman2004431@gmail.com>
2023-12-07 09:14:17 +0200

    feat/RG: add Roman Hrytskovs portfolio on page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import ImagePlacer from "./components/ImagePlacer";
 import DescriptionBox from "./components/DescriptionBox";
 import Card from "./components/Card";
 import HorizontalStack from "./components/HorizontalStack";
-
+import HrytskovImage from "./images/romenjo.jpg";
 
 function App() {
   return (
     <>
       <HorizontalStack>
-        
+        <Card
+          imageUrl={HrytskovImage}
+          name="Roman Hrytskov"
+          description="Student of LPNU, 19, 185. Likes football, adores football, believes in footballs supremacy.
+          Listens to Lil Peep tracks."
+          glowColor="cyan"
+        />
       </HorizontalStack>
     </>
   );
